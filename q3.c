9e603e209dac585d62fafc21135f8e68c8f26719
Jibe7 <84497196+Jibe7@users.noreply.github.com>
2021-12-23 02:11:16 +0100

    Update q3.c
    More functions

--- q3.c
+++ q3.c
@@ -6,27 +6,34 @@
 #include <errno.h>
 #define MAX_SIZE 255 //Max size of command
 
-int main(int argc, char *argv[]) {
+int showmsg(char *msg) { 
+	int len = strlen(msg);
+	write(1, msg, len);
+	return 0;
+}
+
+int welcoming(char *welcsentence, char *exitsentence, char *shellname) { 
+	showmsg(welcsentence);
+	showmsg(exitsentence);
+	showmsg(shellname);	
+	return 0;
+}
+
+int readAndExecv2(int sizeOfCommand) {
 	char *prompt;
 	int ex;
 	int status;
-	
-	write(1,"Welcome to ENSEA Tiny Shell !\n",strlen("Welcome to ENSEA Tiny Shell !\n"));
-	write(1,"Pour quitter, tapez 'exit'.\n",strlen("Pour quitter, tapez 'exit'.\n"));
-	write(1,"enseash % ",strlen("enseash % ")); //For first execution
-	
-	do {
-		prompt = (char *) malloc(MAX_SIZE); //To store what the user will write
+	prompt = (char *) malloc(MAX_SIZE); //To store what the user will write
 		int n = read(0,prompt,MAX_SIZE);
 		prompt[n-1] = '\0'; //To erase the '\n' character made by pressing ENTER
 		
 		//We differientiated both exits just for the property of the shell (new line in this case)
 		if (strcmp(prompt,"exit") == 0) { //strcmp equals 0 if both strings are equal
-			write(1,"Bye bye ...\n",strlen("Bye bye ...\n"));
+			showmsg("Bye bye ...\n");
 			exit(EXIT_SUCCESS); //We exit the program
 		}
 		else if (n == 0) { //This second test is for a <ctrl>+d
-			write(1,"\nBye bye ...\n",strlen("\nBye bye ...\n"));
+			showmsg("\nBye bye ...\n");
 			exit(EXIT_SUCCESS); //We exit the program
 		}
 		
@@ -35,7 +42,7 @@ int main(int argc, char *argv[]) {
 			if (ret == 0) {
 				ex = execlp(prompt,prompt,(char *)NULL); //We execute and save the value returned by execlp
 				if (ex < 0) { //If error occured during execlp
-					write(1,"Error occured, please try again. \n", strlen("Error occured, please try again. \n"));
+					showmsg("Error occured, please try again. \n");
 					exit(EXIT_FAILURE);
 				}
 			}
@@ -44,7 +51,16 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		prompt = (char *) realloc(prompt,MAX_SIZE); //Reallocation of the memory for prompt
-		write(1,"enseash % ",strlen("enseash % "));
+		return 0;
+}
+
+int main(int argc, char *argv[]) {
+	
+	welcoming("Welcome to ENSEA Tiny Shell !\n", "Pour quitter, tapez 'exit'.\n", "enseash % \n");
+	
+	do {
+		readAndExecv2(MAX_SIZE);
+		showmsg("enseash % ");
 	} while(1);
 	
 	return 0;
