fc94c1d668af16afeac3ed035fc3c2c7605d4861
Jibe7 <84497196+Jibe7@users.noreply.github.com>
2021-12-24 15:32:14 +0100

    Update q5.c

--- q5.c
+++ q5.c
@@ -17,65 +17,83 @@ double calculate_time(struct timespec start, struct timespec stop) {
 	return elapsed_time;
 }
 
-int main(int argc, char *argv[]) {
-	char *prompt; //To store what the user will write
+int showmsg(char *msg) { 
+	int len = strlen(msg);
+	write(1, msg, len);
+	return 0;
+}
+
+int welcoming(char *welcsentence, char *exitsentence, char *shellname) { 
+	showmsg(welcsentence);
+	showmsg(exitsentence);
+	showmsg(shellname);	
+	return 0;
+}
+
+int enseashCommands(char *prompt, int n) { //execute the prompt command given in argument, with n the length of the prompt command
 	int ex;
 	int status;
-	char *out; //Will be used to write in the shell
 	struct timespec start, stop;
 	double elapsed_time;
-	
-	write(1,"Welcome to ENSEA Tiny Shell !\n",strlen("Welcome to ENSEA Tiny Shell !\n"));
-	write(1,"Pour quitter, tapez 'exit'.\n",strlen("Pour quitter, tapez 'exit'.\n"));
-	write(1,"enseash % ",strlen("enseash % ")); //For first execution
-	
-	do {
-		prompt = (char *) malloc(MAX_SIZE);
-		out = (char *) malloc(MAX_SIZE);
-		int n = read(0,prompt,MAX_SIZE);
-		prompt[n-1] = '\0'; //To erase the '\n' character made by pressing ENTER
+	char *out; //Will be used to write in the shell
+	out = (char *) malloc(MAX_SIZE);
+	prompt[n-1] = '\0'; //To erase the '\n' character made by pressing ENTER
 		
 		//We differientiated both exits just for the property of the shell (new line in this case)
-		if (strcmp(prompt,"exit") == 0) { //strcmp equals 0 if both strings are equal
-			write(1,"Bye bye ...\n",strlen("Bye bye ...\n"));
-			exit(EXIT_SUCCESS); //We exit the program
-		}
-		else if (n == 0) { //This second test is for a <ctrl>+d
-			write(1,"\nBye bye ...\n",strlen("\nBye bye ...\n"));
-			exit(EXIT_SUCCESS); //We exit the program
-		}
+	if (strcmp(prompt,"exit") == 0) { //strcmp equals 0 if both strings are equal
+		showmsg("Bye bye ...\n");
+		exit(EXIT_SUCCESS); //We exit the program
+	}
+	else if (n == 0) { //This second test is for a <ctrl>+d
+		showmsg("Bye bye ...\n");
+		exit(EXIT_SUCCESS); //We exit the program
+	}
 		
-		else {
-			pid_t ret = fork(); //We fork so that the command will be executed by the son
-			if (ret == 0) {
-				ex = execlp(prompt,prompt,(char *)NULL); //We execute and save the value returned by execlp
-				if (ex < 0) { //If error occured during execlp
-					write(1,"Error occured, please try again. \n", strlen("Error occured, please try again. \n"));
-					exit(EXIT_FAILURE);
-				}
-			}
-			else if (ret > 0) { //If it is the father, he will wait
-				clock_gettime(CLOCK_MONOTONIC, &start);
-				wait(&status);
-				clock_gettime(CLOCK_MONOTONIC, &stop);
-				elapsed_time = calculate_time(start,stop);
+	else {
+		pid_t ret = fork(); //We fork so that the command will be executed by the son
+		if (ret == 0) {
+		ex = execlp(prompt,prompt,(char *)NULL); //We execute and save the value returned by execlp
+		if (ex < 0) { //If error occured during execlp
+			showmsg("Error occured, please try again. \n");
+			exit(EXIT_FAILURE);
 			}
 		}
-		
-		//Now we check the return code of the previously executed command
-		if (WIFEXITED(status)) {
-			sprintf(out,"enseash [exit:%d|%.0fms] %% ",WEXITSTATUS(status),elapsed_time);
+		else if (ret > 0) { //If it is the father, he will wait
+			clock_gettime(CLOCK_MONOTONIC, &start);
+			wait(&status);
+			clock_gettime(CLOCK_MONOTONIC, &stop);
+			elapsed_time = calculate_time(start,stop);
 		}
-		else if (WIFSIGNALED(status)) {
-			sprintf(out,"enseash [sign:%d|%.0fms] %% ",WTERMSIG(status),elapsed_time);
-		}
+	}
 		
-		write(1,out,strlen(out));
-		prompt = (char *) realloc(prompt,MAX_SIZE); //Reallocation of the memory for prompt
-		out = (char *) realloc(out,MAX_SIZE); //Reallocation of the memory for out
+	//Now we check the return code of the previously executed command
+	if (WIFEXITED(status)) {
+		sprintf(out,"enseash [exit:%d|%.0fms] %% ",WEXITSTATUS(status),elapsed_time);
+	}
+	else if (WIFSIGNALED(status)) {
+		sprintf(out,"enseash [sign:%d|%.0fms] %% ",WTERMSIG(status),elapsed_time);
+	}
+		
+	showmsg(out);
+	out = (char *) realloc(out,MAX_SIZE); //Reallocation of the memory for out
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
 	
+	welcoming("Welcome to ENSEA Tiny Shell !\n", "Pour quitter, tapez 'exit'.\n", "enseash % \n");
+	
+	do {
+		char *prompt; //To store what the user will write
+		prompt = (char *) malloc(MAX_SIZE); //To store what the user will write
+		int n = read(0,prompt,MAX_SIZE);
+		prompt[n-1] = '\0'; //To erase the '\n' character made by pressing ENTER
+		enseashCommands(prompt,n);
+		prompt = (char *) realloc(prompt,MAX_SIZE); //Reallocation of the memory for prompt
+		showmsg("enseash % ");
 	} while(1);
 	
 	return 0;
 }
+
 
