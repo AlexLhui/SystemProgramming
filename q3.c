59deeba836c2de1f7f5a5ce619d0d866a2d126d0
Jibe7 <84497196+Jibe7@users.noreply.github.com>
2021-12-24 13:15:27 +0100

    Update q3.c

--- q3.c
+++ q3.c
@@ -24,34 +24,34 @@ int readAndExecv2(int maxSizeOfCommand) {
 	int ex;
 	int status;
 	prompt = (char *) malloc(maxSizeOfCommand); //To store what the user will write
-		int n = read(0,prompt,maxSizeOfCommand);
-		prompt[n-1] = '\0'; //To erase the '\n' character made by pressing ENTER
+	int n = read(0,prompt,maxSizeOfCommand);
+	prompt[n-1] = '\0'; //To erase the '\n' character made by pressing ENTER
 		
-		//We differientiated both exits just for the property of the shell (new line in this case)
-		if (strcmp(prompt,"exit") == 0) { //strcmp equals 0 if both strings are equal
-			showmsg("Bye bye ...\n");
-			exit(EXIT_SUCCESS); //We exit the program
-		}
-		else if (n == 0) { //This second test is for a <ctrl>+d
-			showmsg("\nBye bye ...\n");
-			exit(EXIT_SUCCESS); //We exit the program
-		}
+	//We differientiated both exits just for the property of the shell (new line in this case)
+	if (strcmp(prompt,"exit") == 0) { //strcmp equals 0 if both strings are equal
+		showmsg("Bye bye ...\n");
+		exit(EXIT_SUCCESS); //We exit the program
+	}
+	else if (n == 0) { //This second test is for a <ctrl>+d
+		showmsg("\nBye bye ...\n");
+		exit(EXIT_SUCCESS); //We exit the program
+	}
 		
-		else {
-			pid_t ret = fork(); //We fork so that the command will be executed by the son
-			if (ret == 0) {
-				ex = execlp(prompt,prompt,(char *)NULL); //We execute and save the value returned by execlp
-				if (ex < 0) { //If error occured during execlp
+	else {
+		pid_t ret = fork(); //We fork so that the command will be executed by the son
+		if (ret == 0) {
+			ex = execlp(prompt,prompt,(char *)NULL); //We execute and save the value returned by execlp
+			if (ex < 0) { //If error occured during execlp
 					showmsg("Error occured, please try again. \n");
 					exit(EXIT_FAILURE);
-				}
-			}
-			else if (ret > 0) { //If it is the father, he will wait
-				wait(&status);
 			}
 		}
-		prompt = (char *) realloc(prompt,maxSizeOfCommand); //Reallocation of the memory for prompt
-		return 0;
+		else if (ret > 0) { //If it is the father, he will wait
+			wait(&status);
+		}
+	}
+	prompt = (char *) realloc(prompt,maxSizeOfCommand); //Reallocation of the memory for prompt
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
